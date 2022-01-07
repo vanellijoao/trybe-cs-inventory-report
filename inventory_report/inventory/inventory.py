7d1b8ad7b5f9b5e3694dd5bef92195b9799e520a
Joao Vanelli <vanellijoao@gmail.com>
2022-01-07 18:04:57 -0300

    Req 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ from ..reports.complete_report import CompleteReport
 from ..reports.simple_report import SimpleReport
 import csv
 import json
+import xml.etree.ElementTree as ElementTree
 
 
 class Inventory:
@@ -18,8 +19,21 @@ class Inventory:
             json_file = json.loads(content)
             return json_file
 
+    @staticmethod
+    def import_xml_file(path):
+        tree = ElementTree.parse(path)
+        root = tree.getroot()
+        xml_file = []
+        for item in root:
+            dict_item = {}
+            for child in item:
+                dict_item[child.tag] = child.text
+            xml_file.append(dict_item)
+        return xml_file
+
     def import_data(path, type):
         dict_types = {"simples": SimpleReport, "completo": CompleteReport}
+        file = ""
 
         if path.endswith(".csv"):
             file = Inventory.import_csv_file(path)
@@ -28,3 +42,9 @@ class Inventory:
         elif path.endswith(".json"):
             file = Inventory.import_json_file(path)
             return dict_types[type].generate(file)
+
+        else:
+            file = Inventory.import_xml_file(path)
+            return dict_types[type].generate(file)
+
+        return dict_types[type].generate(file)
